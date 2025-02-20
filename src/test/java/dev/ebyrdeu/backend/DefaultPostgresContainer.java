121cc9b1ab14d88c5dc6be594582fbce9413bc96
Maxim Khnykin <ebyrdeu.khnykin@gmail.com>
2025-02-20 12:27:45 +0100

    updated

--- src/test/java/dev/ebyrdeu/backend/DefaultPostgresContainer.java
+++ src/test/java/dev/ebyrdeu/backend/DefaultPostgresContainer.java
@@ -1,8 +1,6 @@
 package dev.ebyrdeu.backend;
 
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -13,18 +11,5 @@ public class DefaultPostgresContainer {
 	@Container
 	@ServiceConnection
 	private static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:17");
-
-	static {
-		container.withReuse(true);
-		container.start();
-	}
-
-	@DynamicPropertySource
-	private static void configureProperties(DynamicPropertyRegistry registry) {
-		registry.add("spring.datasource.url", container::getJdbcUrl);
-		registry.add("spring.datasource.username", container::getUsername);
-		registry.add("spring.datasource.password", container::getPassword);
-	}
-
 
 }
